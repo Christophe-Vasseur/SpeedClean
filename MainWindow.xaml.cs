66915e8531c24d56e88341853031f702ec02a30e
Christophe-Vasseur <chris.vasseur.dev@gmail.com>
2023-03-12 15:11:24 +0100

    version 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +22,176 @@ namespace SpeedClean
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Dossier du répertoire Windows (en principe c:\Windows\Temp)
+        public DirectoryInfo winTempDirectory;
+
+        // Dossier temporaires des applications de l'utilisateur courant
+        // C:\Users\<nom utilisateur>\AppData\Local\Temp
+        public DirectoryInfo appTempDirectory; 
+
         public MainWindow()
         {
             InitializeComponent();
+            winTempDirectory = new DirectoryInfo(@"C:\Windows\Temp");
+            appTempDirectory = new DirectoryInfo(path: System.IO.Path.GetTempPath());
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // Calcul de la taille d'un dossier
+        public long DirectorySize(DirectoryInfo dir)
         {
+            // calcul la somme de la taille de tous les fichiers à la racine
+            long calcul = dir.GetFiles().Sum(Fi => Fi.Length);
+                        
+            // Boucle sur tous les répertoires
+            // Pour chaque répertoire on calcule la taille de tous ces fichiers
+            
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                try
+                {
+                    calcul += d.EnumerateFiles().Sum(file => file.Length);
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+            
+            return calcul;
+
+        }
+
+        // Supprime les fichier d'un dossier
+        // Retourne le nombre de fichiers supprimes
+        public int ClearDirectory(DirectoryInfo dir)
+        {
+            int nbFileDelete = 0; // Nombre de fichier supprimer
+
+            // Boucle sur les fichiers à la racine
+            // Supprime tous les fichiers à la racine
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    nbFileDelete++;
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+
+            // Boucle sur les dossiers
+            // Supprime les repertoires à la racine 
+            
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                try
+                {
+                    // true indique une suppression récursive (l'ensemble du contenu du dossier)
+                    d.Delete(true);
+                    nbFileDelete++;
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+
+            return nbFileDelete;
+        }
+
+
+        private void Button_Analyser(object sender, RoutedEventArgs e)
+        {
+            Label_Bandeau.Content = "Effacer en un clic vos fichiers temporaires";
+            AnalyzeFolders();
+        }
+
+        private void Button_Nettoyer(object sender, RoutedEventArgs e)
+        {
+            long nbFichier = 0;
+            
+
+            Label_Bandeau.Content = "        *** NETTOYAGE EN COURS ***";
+
+            // Nettoyer le presse-papier
+            if (Checkbox_Presse_Papier.IsChecked == true)
+            {
+                Clipboard.Clear();
+            }
+
+            
+            try
+            {
+                nbFichier = ClearDirectory(winTempDirectory);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+            
+            
+            try
+            {
+                nbFichier += ClearDirectory(appTempDirectory);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+            
+
+            Label_Bandeau.Content = "        *** NETTOYAGE TERMINE ***";
+            label_Infos.Content = nbFichier.ToString() + " fichiers supprimés !";
+            Btn_Nettoyer.IsEnabled = false;
+
+
+        }
+
+        private void Button_Goto_Site(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("https://www.eugenetoons.fr")
+                {
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Navigateur non trouvé !",  "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Goto_Github(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("https://github.com/Christophe-Vasseur/SpeedClean")
+                {
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Navigateur non trouvé !", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // Analyse les dossiers temporaires
+        // Calcul de la taille totale
+        public void AnalyzeFolders()
+        {           
+
+            long totalSize = 0;    // Taille totale qui peut être nettoyée
+
+            totalSize = DirectorySize(winTempDirectory) / 1000000L;
+            totalSize += DirectorySize(appTempDirectory) / 1000000L;
+
+            label_Infos.Content = totalSize.ToString() + " MB (Fichiers & Dossiers)";
+            Btn_Nettoyer.IsEnabled = true;
+
 
         }
     }
--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # SpeedClean
-(--- LOGICIEL NON FONCTIONNEL ET EN COURS DE DEVELOPPEMENT ---)
---
+
 ## Introduction
 Ce logiciel est un utilitaire développé en C# qui permet de supprimer les fichiers temporaires sous Windows 10/11.
+
 ## Fonctionnement
-- Télécharger et lancer le fichier "SpeedClean.exe".
+- Télécharger et lancer le fichier **"SpeedClean.exe"**.
 - Cliquer sur le bouton "Analyser" et vous verrez apparaitre la taille des fichiers temporaires.
-- CLiquer sur "Nettoyer" pour supprimer définitivement l'ensemble des fichiers temporaires."
+- Cliquer sur "Nettoyer" pour supprimer définitivement l'ensemble des fichiers temporaires."
+
+## Prochaines fonctionnalités
+- Vider la corbeille de Windows
+- Vider le spooler de l'imprimante
+- Vider le cache de Chrome
+- Vider le cache de Edge
+- Vider le cache de FireFox
+- Vider le cache de Safari
+- Vider le cache de Brave
+- Vider le cache de Safari
+- Vider le cache d'Internet Explorer
+ 
+## Les versions du logiciel
+
+### Version 1 : 10/03/2023
+- Efface le contenu temporaire du dossier des applications de Windows
+- Efface le contenu temporaire du dossier des applications de l'utilisateur
+- Vide le presse-papier
